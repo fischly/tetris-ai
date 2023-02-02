791401f2e7fbd922cdde3d6b3483f966c9938efa
fischly <felix.mitterer@posteo.de>
2023-02-02 15:19:03 +0100

    refactoring (splitting model and other util in own files)

---
+++ dqnmodel.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import random
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class DQNModel():
+    def __init__(self, update_frequency, load_from_file=None):
+        self.update_frequency = update_frequency
+        self.update_counter = 0
+        
+        self.model = self.create_model()
+        self.target_model = self.create_model()
+        
+        if load_from_file is not None:
+            self.model.load_state_dict(torch.load(load_from_file))
+        
+        # make the parameters of both models the same to start with
+        self.update_target()
+        
+    def create_model(self):
+        # layer initialisation
+        def init_linear_layer(m, method):
+            torch.nn.init.xavier_normal_(m.weight, nn.init.calculate_gain(method))
+            torch.nn.init.constant_(m.bias, 0)
+            return m
+                
+        model = nn.Sequential(
+            init_linear_layer(nn.Linear(20*10, 128), 'relu'),
+            nn.ReLU(),
+            init_linear_layer(nn.Linear(128, 64), 'relu'),
+            nn.ReLU(),
+            init_linear_layer(nn.Linear(64, 32), 'relu'),
+            nn.ReLU(),
+            init_linear_layer(nn.Linear(32, 1), 'linear')
+        )
+        
+        return model
+    
+    def update_target(self):
+        for param, target_param in zip(self.target_model.parameters(), self.model.parameters()):
+            target_param.data.copy_(param)
+    
+    def step(self):
+        self.update_counter += 1
+        
+        if self.update_counter >= self.update_frequency:
+            print(f'[DQNModel] updating target network')
+            self.update_counter -= self.update_frequency
+            
+            self.update_target()
+    
+    
+    def to(self, device):
+        self.model.to(device)
+        self.target_model.to(device)
